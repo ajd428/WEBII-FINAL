dd0e5bf6d2ee11efac87a5e321d5f6051e991a98
ajd428 <102680878+ajd428@users.noreply.github.com>
2022-06-09 00:53:30 -0400

    added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,22 @@
+let burgerBtn = document.querySelector("#burger-btn");
+let mainMenu = document.querySelector("#mainmenu");
 
+let canSeeMenu = false;
+
+burgerBtn.addEventListener("click", () => {
+
+    if(!canSeeMenu){
+        mainMenu.classList.add("show-menu");
+        canSeeMenu = true;
+    }else{
+        mainMenu.classList.remove("show-menu");
+        canSeeMenu = false;
+    }
+})
+
+let closeBtn = document.querySelector("#close-btn");
+
+closeBtn.addEventListener("click", () => {
+    mainMenu.classList.remove("show-menu");
+    canSeeMenu = false;
+})
